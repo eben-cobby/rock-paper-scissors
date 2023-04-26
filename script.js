869116fbf7efdc271906be69d36a2efd4a77775c
eben_cobby <104040859+eben-cobby@users.noreply.github.com>
2023-04-26 23:24:05 +0000

    add single play function

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 // create an array of possible choices
 const choices = ["Rock", "Paper", "Scissors"];
 
-// function for computer to randomly make a choice
+// computer randomly makes a choice
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * choices.length);
-    let choice = choices[randomIndex];
-    console.log(choice);
+    let computerChoice = choices[randomIndex];
+    return computerChoice;
 }
 
-getComputerChoice()
+//gets user's choice
+function getPlayerChoice() {
+    let userChoice = prompt("Rock? Paper? Scissors???").toLowerCase(); // take case-insensitive user choice
+    return (userChoice.charAt(0).toUpperCase() + userChoice.slice(1)); //case first letter
+}
+
+// play single round of game
+function playRound(playerSelection, computerSelection) {
+    let tie = playerSelection === computerSelection
+    let playerWIn = (playerSelection === "Rock" && computerSelection === "Scissors")
+        || (playerSelection === "Paper" && computerSelection === "Rock")
+        || (playerSelection === "Scissors" && computerSelection === "Paper")
+    if (tie) {
+        return `It's a tie! You both chose ${playerSelection}`
+    } else {
+        return playerWIn ? `You win! ${playerSelection} beats ${computerSelection}!` : `You lose! ${computerSelection} beats ${playerSelection}!`
+    }
+}
+// play multiple rounds
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 1; i <= 5; i++) {
+        result = playRound(getPlayerChoice(), getComputerChoice())
+    }
+    if (playerScore === computerScore)
+}
+
+game();
